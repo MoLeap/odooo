67c29598850febb45d8b0c6442cd0094be9c5a40
chha-odoo <chha@odoo.com>
2025-10-31 18:46:14 +0530

    [IMP] purchase_stock: prefer multiple(replenishment_uom_id) in pol
    - Currently, the system always used the `unit` defined on the price rule
      (`supplier_id`), even when a different unit was selected in
      `replenishment_uom_id` on orderpoint while creating pol.
    
    With this change, if a Multiple(`replenishment_uom_id`) is set, it will
    be used as the unit on the purchase order line. As a result, users can
    purchase products in the exact units they want, improving flexibility
    and alignment with their purchasing preferences.
    
    TaskId : 5047947

--- addons/stock/models/stock_orderpoint.py
+++ addons/stock/models/stock_orderpoint.py
@@ -739,8 +739,14 @@ class StockWarehouseOrderpoint(models.Model):
                             if global_horizon_days:
                                 date -= relativedelta.relativedelta(days=int(global_horizon_days))
                             values = orderpoint._prepare_procurement_values(date=date)
+                            qty_to_order = orderpoint.qty_to_order
+                            product_uom = orderpoint.uom_id
+                            if orderpoint.replenishment_uom_id and orderpoint.replenishment_uom_id != product_uom:
+                                values['force_uom'] = True
+                                qty_to_order = product_uom._compute_quantity(orderpoint.qty_to_order, orderpoint.replenishment_uom_id)
+                                product_uom = orderpoint.replenishment_uom_id
                             procurements.append(self.env['stock.rule'].Procurement(
-                                orderpoint.product_id, orderpoint.qty_to_order, orderpoint.uom_id,
+                                orderpoint.product_id, qty_to_order, product_uom,
                                 orderpoint.location_id, orderpoint.name, origin,
                                 orderpoint.company_id, values))
 
