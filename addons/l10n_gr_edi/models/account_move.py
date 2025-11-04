1c92d6dfdcaf482176bf24e0040fd9e72049ad94
smjo-odoo <smjo@odoo.com>
2025-11-04 14:57:32 +0530

    [ADD] l10n_gr_edi: implement CIUS for Greece
    In this commit:
    
    - Implemented Greek CIUS (core invoice usage specification based on BIS-3)
      which is mandatory for all B2G e-invoices being sent via Peppol.
    
    Task: 4981749

--- addons/l10n_gr_edi/models/account_move.py
+++ addons/l10n_gr_edi/models/account_move.py
@@ -86,6 +86,22 @@ class AccountMove(models.Model):
         compute='_compute_from_l10n_gr_edi_document_ids',
         store=True,
     )
+    l10n_gr_edi_budget_type = fields.Selection([
+        ('1', '1 - Ordinary Budget'),
+        ('2', '2 - PDE (Public Investment Budget)'),
+        ('3', '3 - Other Budget')
+        ], string="Budget Type"
+    )
+    l10n_gr_edi_project_reference = fields.Char(
+        string='Project reference number',
+        help='ADA or Enaritmos number',
+    )
+    l10n_gr_edi_contract_reference = fields.Char(
+        string='Contract ADAM',
+        help='Internet contract posting number(ADAM) of the Central Electronic Register of Public Procurement (KIMDIS)',
+        default='0',
+        required=True,
+    )
 
     def _auto_init(self):
         """
@@ -786,3 +802,19 @@ class AccountMove(models.Model):
             raise UserError(_("Some of the selected moves does not meet the requirements to be sent to myDATA."))
 
         self.l10n_gr_edi_try_send_expense_classification()
+
+    ################################################################################
+    # Peppol/UBL helpers
+    ################################################################################
+
+    def _need_ubl_cii_xml(self, ubl_cii_format):
+        """
+        Override the generic check to postpone the creation of the UBL file
+        for Greek B2G invoices until we actually receive the `l10n_gr_edi_mark`
+        from myDATA.
+        """
+        if not super()._need_ubl_cii_xml(ubl_cii_format):
+            return False
+        if ubl_cii_format == 'ubl_gr':
+            return bool(self.l10n_gr_edi_mark)
+        return True
