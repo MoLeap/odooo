5f253c9b2385934fc23edc475f5535d0bd1145bc
sbbh-odoo <sbbh@odoo.com>
2025-12-23 18:03:28 +0530

    [REF] html_(builder|editor): fix focus handling in toolbar dropdown buttons
    Description of the change:
    
    - ListSelector, AlignSelector, and TableAlignSelector now uses DropdownItem,
      enabling proper focus movement and tab navigation.
    - When a dropdown is open, pressing Escape closes it and returns focus
      to the parent dropdown button.
    
    task-5155813

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -41,7 +41,6 @@ import { FONT_SIZE_CLASSES, getFontSizeOrClass, getHtmlStyle } from "@html_edito
 import { getTextColorOrClass, TEXT_CLASSES_REGEX } from "@html_editor/utils/color";
 import { baseContainerGlobalSelector } from "@html_editor/utils/base_container";
 import { ListSelector } from "./list_selector";
-import { reactive } from "@odoo/owl";
 import { composeToolbarButton } from "../toolbar/toolbar";
 import { isHtmlContentSupported } from "@html_editor/core/selection_plugin";
 import { pick } from "@web/core/utils/objects";
@@ -85,7 +84,6 @@ export class ListPlugin extends Plugin {
     static defaultConfig = {
         allowChecklist: true,
     };
-    toolbarListSelectorKey = reactive({ value: 0 });
     /** @type {import("plugins").EditorResources} */
     resources = {
         user_commands: [
@@ -153,7 +151,6 @@ export class ListPlugin extends Plugin {
                 props: {
                     getButtons: () => this.listSelectorButtons,
                     getListMode: this.getListMode.bind(this),
-                    key: this.toolbarListSelectorKey,
                 },
                 isAvailable: this.canToggleList.bind(this),
             }),
@@ -182,7 +179,6 @@ export class ListPlugin extends Plugin {
 
         /** Handlers */
         normalize_handlers: this.normalize.bind(this),
-        step_added_handlers: this.updateToolbarButtons.bind(this),
         delete_handlers: this.adjustListPaddingOnDelete.bind(this),
 
         /** Overrides */
@@ -1300,10 +1296,6 @@ export class ListPlugin extends Plugin {
     // --------------------------------------------------------------------------
     // Toolbar buttons
     // --------------------------------------------------------------------------
-
-    updateToolbarButtons() {
-        this.toolbarListSelectorKey.value++;
-    }
 
     getListSelectorButtons() {
         return listSelectorItems
