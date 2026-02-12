2768a0bb6826ef7acdd25c5e0785b582e53b8fa0
Patricia Daloze (pda) <pda@odoo.com>
2026-02-12 11:01:26 +0100

    [IMP] fixes for review

--- addons/delivery/models/payment_transaction.py
+++ addons/delivery/models/payment_transaction.py
@@ -12,7 +12,7 @@ class PaymentTransaction(models.Model):
         cod_pending_txs = self.filtered(
             lambda tx: tx.provider_id.custom_mode == "cash_on_delivery" and tx.state == "pending"
         )
-        cod_pending_txs.sale_order_ids.filtered(lambda so: so.state == "draft").with_context(
+        cod_pending_txs.sale_order_ids.filtered(lambda so: so.state in ["draft", "sent"]).with_context(
             send_email=True
         ).action_confirm()
         super()._post_process()
