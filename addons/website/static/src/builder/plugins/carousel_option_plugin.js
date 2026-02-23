eff295eb250ea79cceb3d70430a7f4f3bc833778
Robin Lejeune (role) <role@odoo.com>
2026-02-23 11:27:30 +0100

    [FIX] website: set aria on carousel elements
    In an effort to improve the accessibility of our carousels, this commit
    sets proper aria roles, roledescription and other tied attributes on
    the carousels, their controllers and their slides.
    
    See [WAI-ARIA carousel example](https://www.w3.org/WAI/ARIA/apg/patterns/carousel/examples/carousel-2-tablist/)
    
    task-5470023

--- addons/website/static/src/builder/plugins/carousel_option_plugin.js
+++ addons/website/static/src/builder/plugins/carousel_option_plugin.js
@@ -49,6 +49,7 @@ export class CarouselOptionPlugin extends Plugin {
         },
         builder_actions: {
             AddSlideAction,
+            SetAutoplayAction,
             SlideCarouselAction,
             ToggleControllersAction,
             ToggleCardImgAction,
@@ -73,9 +74,9 @@ export class CarouselOptionPlugin extends Plugin {
             });
             carouselEl.querySelectorAll(".carousel-indicators > *").forEach((indicatorEl, i) => {
                 indicatorEl.classList.toggle("active", i === 0);
-                indicatorEl.removeAttribute("aria-current");
+                indicatorEl.setAttribute("aria-selected", "false");
                 if (i === 0) {
-                    indicatorEl.setAttribute("aria-current", "true");
+                    indicatorEl.setAttribute("aria-selected", "true");
                 }
             });
         }
@@ -101,6 +102,7 @@ export class CarouselOptionPlugin extends Plugin {
             activateClone: false,
         });
         newItemEl.classList.remove("active");
+        newItemEl.id = `${editingElement.id}_${Date.now().toString(36)}`;
 
         // Show the controllers (now that there is always more than one item).
         const controlEls = editingElement.querySelectorAll(carouselControlsSelector);
@@ -109,11 +111,14 @@ export class CarouselOptionPlugin extends Plugin {
         });
 
         // Add the new indicator.
-        const indicatorsEl = editingElement.querySelector(".carousel-indicators");
-        const newIndicatorEl = this.document.createElement("button");
-        newIndicatorEl.setAttribute("data-bs-target", "#" + editingElement.id);
+        const activeIndicatorEl = editingElement.querySelector(
+            ".carousel-indicators button.active"
+        );
+        const newIndicatorEl = activeIndicatorEl.cloneNode(true);
+        newIndicatorEl.setAttribute("aria-controls", newItemEl.id);
         newIndicatorEl.setAttribute("aria-label", _t("Carousel indicator"));
-        indicatorsEl.appendChild(newIndicatorEl);
+        newIndicatorEl.setAttribute("aria-selected", "false");
+        activeIndicatorEl.after(newIndicatorEl);
 
         // Slide to the new item.
         await this.slide(editingElement, "next");
@@ -193,7 +198,7 @@ export class CarouselOptionPlugin extends Plugin {
                         );
                         const activeIndicatorEl = [...indicatorEls][activeIndex];
                         activeIndicatorEl.classList.add("active");
-                        activeIndicatorEl.setAttribute("aria-current", "true");
+                        activeIndicatorEl.setAttribute("aria-selected", "true");
 
                         // Activate the active item.
                         this.dependencies["builderOptions"].setNextTarget(activeItemEl);
@@ -305,9 +310,9 @@ export function updateCarouselIndicators(carouselEl, newPosition) {
     const indicatorEls = carouselEl.querySelectorAll(".carousel-indicators > *");
     indicatorEls.forEach((indicatorEl, i) => {
         indicatorEl.classList.toggle("active", i === newPosition);
-        indicatorEl.removeAttribute("aria-current");
+        indicatorEl.setAttribute("aria-selected", "false");
         if (i === newPosition) {
-            indicatorEl.setAttribute("aria-current", "true");
+            indicatorEl.setAttribute("aria-selected", "true");
         }
     });
 }
@@ -321,6 +326,18 @@ export class AddSlideAction extends BuilderAction {
         return this.dependencies.carouselOption.addSlide(editingElement);
     }
 }
+
+export class SetAutoplayAction extends BuilderAction {
+    static id = "setAutoplay";
+    isApplied({ editingElement, params: { bsRide } }) {
+        return editingElement.dataset.bsRide === bsRide;
+    }
+    apply({ editingElement, params: { bsRide, ariaLive } }) {
+        editingElement.dataset.bsRide = bsRide;
+        editingElement.querySelector(".carousel-inner")?.setAttribute("aria-live", ariaLive);
+    }
+}
+
 export class SlideCarouselAction extends BuilderAction {
     static id = "slideCarousel";
     static dependencies = ["carouselOption"];
