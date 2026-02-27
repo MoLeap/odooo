ce6d5d1bc9d551ab167e5180b9bff21d1085a430
bona-odoo <bona@odoo.com>
2026-02-27 07:26:30 +0000

    [FIX] hr_recruitment: prevent displaying archived email templates
    Pre-requisites:
    ---------------
    1. Create or duplicate any `hr.applicant` email template.
    2. Archive the newly created template.
    3. Archive the email template linked to a refuse reason.
    
    Steps to reproduce:
    -------------------------
    1. Install hr_recruitment.
    2. Go to Recruitment > Applications > All Applications and open an applicant.
    3. Click on the "Refuse" button to open the refuse wizard.
    4. Click on the "Email Template"  and click on 'Search More'
    5. Observe available templates
    
    Issue:
    -------
    If a refuse reason is linked to an archived email template,
    the wizard automatically pre-fills that archived template
    
    Cause:
    ---------
    The _compute_template_id method automatically assigns the template
    from the refuse reason without verifying whether the template is
    active, allowing archived templates to be pre-filled in the wizard.
    https://github.com/odoo/odoo/blob/aa2a7c0e5a5de970cdb8f6a7ba9f02ad75cf5078/addons/hr_recruitment/wizard/applicant_refuse_reason.py#L91-L96
    
    Solution:
    -----------
    - Update _compute_template_id to ensure only active templates are
      automatically assigned.
    
    opw-5974244
    
    X-original-commit: 93b9b94eec1cd334a8d5f7020f4486f875de8a36

--- addons/hr_recruitment/wizard/applicant_refuse_reason.py
+++ addons/hr_recruitment/wizard/applicant_refuse_reason.py
@@ -46,7 +46,7 @@ class ApplicantGetRefuseReason(models.TransientModel):
     def _compute_send_mail(self):
         for wizard in self:
             template = wizard.refuse_reason_id.template_id
-            wizard.send_mail = template and not wizard.applicant_without_email
+            wizard.send_mail = template.active and not wizard.applicant_without_email
 
     @api.depends('applicant_ids')
     def _compute_applicant_without_email(self):
@@ -86,8 +86,8 @@ class ApplicantGetRefuseReason(models.TransientModel):
     @api.depends('refuse_reason_id')
     def _compute_template_id(self):
         for wizard in self:
-            if wizard.refuse_reason_id:
-                wizard.template_id = wizard.refuse_reason_id.template_id
+            if wizard.refuse_reason_id and (template := wizard.refuse_reason_id.template_id):
+                wizard.template_id = template.active and template
             else:
                 wizard.template_id = False
 
