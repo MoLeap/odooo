587d0f77df8f4bac8ed997284fa2b0dbd3d6e3a9
Serhii Rubanskyi <seru@odoo.com>
2026-03-10 10:17:38 +0100

    [IMP] website: add draft preview mode
    This commit adds a draft mode for website. All changes you make in this
    mode will be saved as a draft only, and won't be visible to your users
    until you publish the changes.
    
    We believe this option might be advanced for the end-user, so by
    default, this mode isn't enabled. To enable it, you must toggle the
    setting in the settings.
    
    Right now, there's 2 options for publishing/deleting the draft:
    - doing it for the whole website
    - doing it for the page + shared components (header, styles, etc)
    
    There's still some things that aren't working well with this option:
    - Translation, you can't edit the draft content
    - Search (it will search only published things)
    - Widget values (e.g. product price)
    
    task-5440958

--- addons/website/static/tests/builder/website_helpers.js
+++ addons/website/static/tests/builder/website_helpers.js
@@ -53,6 +53,13 @@ class IrUiView extends models.Model {
     }
 }
 
+class ResConfigSettings extends models.Model {
+    _name = "res.config.settings";
+    is_draft_preview_enabled() {
+        return false;
+    }
+}
+
 export const setupWebsiteBuilderOeId = 539;
 
 export const invisibleEl =
@@ -61,7 +68,7 @@ export const invisibleEl =
 export function defineWebsiteModels() {
     describe.current.tags("desktop");
     defineMailModels();
-    defineModels([Website, IrUiView]);
+    defineModels([Website, IrUiView, ResConfigSettings]);
     onRpc("/website/theme_customize_data_get", () => []);
     onRpc("website", "web_search_read", () => ({
         length: 1,
@@ -486,6 +493,7 @@ export const websiteServiceInTranslateMode = {
             metadata: {},
         },
     ],
+    getCanHaveDraftPreview: () => false,
 };
 
 export async function setupSidebarBuilderForTranslation(options) {
