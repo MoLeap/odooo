0288561c8636166b24cff8389506f5dd0824eec0
alan-odoo <alan@odoo.com>
2026-03-11 15:13:48 +0100

    [IMP] calendar: create the draft status
    This commit creates a draft state for the calendar events. This state aims to
    allow users to create events without confirmed them directly and notifying
    users. So it allows users to take time to fill the event with correct
    information and avoid spamming attendees with updates.
    
    This state is only available at the creation, in the advanced form opened from
    the quick create form. Once the draft state is removed, the attendees can be
    notified. It cannot be restored once it has been removed to prevent
    notification issues. Otherwise, it could, for example, prevent update messages
    from being sent or be used as cancellation state which it is not as it does
    not send cancellation messages.
    
    The default value of CalendarEvent.is_draft is False as it slows down events
    process. It is assumed that users generally have all the necessary information
    when creating an event.
    
    Recurrences are allowed for the draft events as users may want to prepare a
    recurring event with missing information. If they wish to remove the state,
    they can massively do it thanks to the is_draft field inside the advance form.
    This field is only present when is_draft is True because a confirmed event
    cannot become a draft again.
    
    The draft events are not included in the Google and Microsoft calendars'
    synchronisation since those external calendars do not have a similar state.
    Otherwise, they could send notifications to the attendees, whereas the event
    is not confirmed yet.
    
    The draft events are faded in the gantt and calendar views to warn users that
    they are not yet confirmed.

--- addons/calendar/models/calendar_alarm_manager.py
+++ addons/calendar/models/calendar_alarm_manager.py
@@ -164,6 +164,8 @@ class CalendarAlarm_Manager(models.AbstractModel):
                 ON event_alarm_rel.calendar_alarm_id = alarm.id
              WHERE alarm.alarm_type = %s
                AND event.active
+               AND event.is_draft IS FALSE 
+                OR event.is_draft IS NULL
                AND event.start - CAST(alarm.duration || ' ' || alarm.interval AS Interval) >= %s
                AND event.start - CAST(alarm.duration || ' ' || alarm.interval AS Interval) < %s
                %s
