aa35659023f7ff64d30c709d97085394efdd761f
alan-odoo <alan@odoo.com>
2026-03-11 15:13:48 +0100

    [IMP] calendar: create the draft status
    This commit creates a draft state for the calendar events. This state aims to
    allow users to create events without confirmed them directly and notifying
    users. So it allows users to take time to fill the event with correct
    information and avoid spamming attendees with updates.
    
    This state is only available at the creation, in the advanced form opened from
    the quick create form. Once the draft state is removed, the attendees can be
    notified. It cannot be restored once it has been removed to prevent
    notification issues. Otherwise, it could, for example, prevent update messages
    from being sent or be used as cancellation state which it is not as it does
    not send cancellation messages.
    
    The default value of CalendarEvent.is_draft is False as it slows down events
    process. It is assumed that users generally have all the necessary information
    when creating an event.
    
    Recurrences are allowed for the draft events as users may want to prepare a
    recurring event with missing information. If they wish to remove the state,
    they can massively do it thanks to the is_draft field inside the advance form.
    This field is only present when is_draft is True because a confirmed event
    cannot become a draft again.
    
    The draft events are not included in the Google and Microsoft calendars'
    synchronisation since those external calendars do not have a similar state.
    Otherwise, they could send notifications to the attendees, whereas the event
    is not confirmed yet.
    
    The draft events are faded in the gantt and calendar views to warn users that
    they are not yet confirmed.

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -167,6 +167,7 @@ class CalendarEvent(models.Model):
         help="""When synchronization with an external calendar is active, this description is synchronized \
         with the one of the associated meeting in that external calendar. Any update will be propagated there \
         and vice versa.""")
+    is_draft = fields.Boolean()
     user_id = fields.Many2one('res.users', 'Organizer', default=lambda self: self.env.user, index='btree_not_null')
     partner_id = fields.Many2one(
         'res.partner', string='Scheduled by', related='user_id.partner_id', readonly=True)
@@ -967,9 +968,10 @@ class CalendarEvent(models.Model):
 
         current_attendees = self.filtered('active').attendee_ids
         skip_attendee_notification = self.env.context.get('skip_attendee_notification')
-        if not skip_attendee_notification and 'partner_ids' in values:
+        invited_attendees = self._get_new_invited_attendees(current_attendees, previous_attendees, vals)
+        if not skip_attendee_notification and invited_attendees:
             # we send to all partners and not only the new ones
-            self._get_new_invited_attendees(current_attendees, previous_attendees, vals)._send_invitation_emails()
+            invited_attendees._send_invitation_emails()
         if not skip_attendee_notification and not self.env.context.get('is_calendar_event_new') and 'start' in values:
             start_date = fields.Datetime.to_datetime(values.get('start'))
             # Only notify on future events
@@ -1081,6 +1083,10 @@ class CalendarEvent(models.Model):
         for old_event, new_event in zip(self, new_events):
             new_event.write({'partner_ids': [(Command.set(old_event.partner_ids.ids))]})
         return new_events
+
+    def action_confirm(self):
+        self.ensure_one()
+        self.is_draft = False
 
     def action_unlink_event(self, attendee_id=None, recurrence=False):
         """
@@ -1782,9 +1788,9 @@ class CalendarEvent(models.Model):
         return Markup("<br/>").join(contact_description)
 
     def _get_new_invited_attendees(self, current_attendees, previous_attendees, vals):
-        """Get the attendees who must receive an invitation for a modified calendar event. This method is meant
-        to be overridden."""
-        return current_attendees - previous_attendees
+        """Get the attendees who must receive an invitation for a modified calendar event. All of them must get it
+        when the draft state is removed as this one previously prevented invitations from being sent."""
+        return current_attendees if vals.get('is_draft') is False else current_attendees - previous_attendees
 
     @api.model
     def _prepare_partner_contact_details_html(self, section_title, partner):
