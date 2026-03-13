069730fcd028aed5036cc75bd8f72a1036b673fb
nd-dew <pian@odoo.com>
2026-03-13 11:39:00 +0100

    [IMP] base: add expliactions on the action.action

--- odoo/addons/base/models/ir_actions.py
+++ odoo/addons/base/models/ir_actions.py
@@ -70,7 +70,7 @@ class IrActionsActions(models.Model):
     help = fields.Html(string='Action Description',
                        help='Optional help text for the users with a description of the target view, such as its usage and purpose.',
                        translate=True)
-    explication = fields.Text(string='Explication')
+    explication = fields.Text(string='AI Explication', help='Verbose, LLM-targeted description of what this action actually does.')
     binding_model_id = fields.Many2one('ir.model', ondelete='cascade',
                                        help="Setting a value makes this action available in the sidebar for the given model.")
     binding_type = fields.Selection([('action', 'Action'),
