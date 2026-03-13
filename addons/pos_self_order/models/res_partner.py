5b62a42cfde8f5c1855fef66ea223259b4a672ae
Léopold Cantraine <lecan@odoo.com>
2026-03-13 16:44:41 +0100

    [IMP] pos*: general improvements for orders
    task: 6009872

--- addons/pos_self_order/models/res_partner.py
+++ addons/pos_self_order/models/res_partner.py
@@ -12,6 +12,6 @@ class ResPartner(models.Model):
     @api.model
     def _load_pos_self_data_read(self, records, config):
         """ Read specific fields from the given records """
-        fields = ['id', 'name', 'write_date', 'property_product_pricelist']
+        fields = ['id', 'name', 'phone', 'street', 'city', 'zip', 'write_date', 'property_product_pricelist']
         records = records.read(fields, load=False)
         return records or []
