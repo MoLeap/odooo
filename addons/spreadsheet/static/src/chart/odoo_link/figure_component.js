fc573ce063991fd1345bda34d1d48b667f61c0f6
Marceline (matho) <matho@odoo.com>
2026-03-18 09:00:55 +0100

    [IMP] spreadsheet: annotation tool on odoo charts
    Task: 5365254

---
+++ addons/spreadsheet/static/src/chart/odoo_link/figure_component.js
@@ -0,0 +1,42 @@
+import { patch } from "@web/core/utils/patch";
+import * as spreadsheet from "@odoo/o-spreadsheet";
+import { useService } from "@web/core/utils/hooks";
+import { navigateToOdoolinkFromChart } from "../odoo_chart/odoo_chart_helpers";
+
+patch(spreadsheet.components.ScorecardChart.prototype, {
+    setup() {
+        super.setup();
+        this.actionService = useService("action");
+        this.notificationService = useService("notification");
+    },
+    async navigateToOdooLink(newWindow) {
+        await navigateToOdoolinkFromChart(this.env, this.props.chartId, newWindow);
+    },
+    get hasOdooLink() {
+        return this.env.model.getters.getChartOdooLink(this.props.chartId) !== undefined;
+    },
+    async onClick() {
+        if (this.env.isDashboard() && this.hasOdooLink) {
+            await this.navigateToOdooLink();
+        }
+    },
+});
+
+patch(spreadsheet.components.GaugeChartComponent.prototype, {
+    setup() {
+        super.setup();
+        this.actionService = useService("action");
+        this.notificationService = useService("notification");
+    },
+    async navigateToOdooLink(newWindow) {
+        await navigateToOdoolinkFromChart(this.env, this.props.chartId, newWindow);
+    },
+    get hasOdooLink() {
+        return this.env.model.getters.getChartOdooLink(this.props.chartId) !== undefined;
+    },
+    async onClick() {
+        if (this.env.isDashboard() && this.hasOdooLink) {
+            await this.navigateToOdooLink();
+        }
+    },
+});
