f895053a5485b1d4fb11824ff6d3464ec33c267a
Benjamin Vray <bvr@odoo.com>
2026-03-23 16:31:44 +0100

    [WIP] build homepage preview

--- addons/website/static/src/client_actions/configurator/configurator.js
+++ addons/website/static/src/client_actions/configurator/configurator.js
@@ -22,7 +22,6 @@ import { registry } from "@web/core/registry";
 import { rpc } from "@web/core/network/rpc";
 import { mixCssColors } from "@web/core/utils/colors";
 import { router } from "@web/core/browser/router";
-import { loadBundle } from "@web/core/assets";
 import { Component, onMounted, onWillStart, onWillUnmount } from "@odoo/owl";
 import { standardActionServiceProps } from "@web/webclient/actions/action_service";
 import { fuzzyLevenshteinLookup } from "@web/core/utils/search";
@@ -651,20 +650,9 @@ export class ThemeSelectionScreen extends ApplyConfiguratorScreen {
     }
 
     async chooseTheme(themeName) {
-        this.uiService.block();
         this.state.previewIsLoading = true;
         this.state.selectedTheme = themeName;
-        this.state.themePreviewBody = await this.orm.call(
-            "website",
-            "configurator_theme_preview_body",
-            [],
-            {
-                theme_name: themeName,
-                install_theme: true,
-            }
-        );
         this.props.navigate(ROUTES.setupStyleScreen);
-        this.uiService.unblock();
     }
 
     async getThemes() {
@@ -695,25 +683,13 @@ export class SetupStyleScreen extends ApplyConfiguratorScreen {
     static template = "website.Configurator.SetupStyleScreen";
     setup() {
         super.setup();
-        this.uiService = useService("ui");
-        this.orm = useService("orm");
         this.state = useStore();
+        this.previewDevice = useState({ value: "desktop" });
         this.previewIframeRef = useRef("previewIframe");
         onWillStart(async () => {
             this.state.previewIsLoading = true;
             if (!this.state.selectedTheme) {
                 return;
-            }
-            if (!this.state.themePreviewBody) {
-                this.state.themePreviewBody = await this.orm.call(
-                    "website",
-                    "configurator_theme_preview_body",
-                    [],
-                    {
-                        theme_name: this.state.selectedTheme,
-                        install_theme: true,
-                    }
-                );
             }
         });
     }
@@ -732,6 +708,15 @@ export class SetupStyleScreen extends ApplyConfiguratorScreen {
 
     setFont(font) {
         this.state.selectedFont = font;
+        this.applyPreviewFont();
+    }
+
+    setPreviewDevice(device) {
+        this.previewDevice.value = device;
+    }
+
+    get isPreviewMobile() {
+        return this.previewDevice.value === "mobile";
     }
 
     async startBuilding() {
@@ -750,12 +735,12 @@ export class SetupStyleScreen extends ApplyConfiguratorScreen {
     }
 
     async onPreviewIframeLoad() {
-        this.uiService.block();
         const iframeDoc = this.previewIframeRef.el?.contentDocument;
-        await loadBundle("web.assets_frontend", { targetDoc: iframeDoc, js: false });
-        this.deactivatePreviewInteractions(iframeDoc);
+        if (iframeDoc) {
+            this.deactivatePreviewInteractions(iframeDoc);
+            this.applyPreviewFont(iframeDoc);
+        }
         this.state.previewIsLoading = false;
-        this.uiService.unblock();
     }
 
     deactivatePreviewInteractions(iframeDoc) {
@@ -767,33 +752,20 @@ export class SetupStyleScreen extends ApplyConfiguratorScreen {
         iframeDoc.addEventListener("submit", stopInteraction, true);
     }
 
-    get previewSrcdoc() {
-        // const p = this.state.selectedPalette;
-        const previewBody = this.state.themePreviewBody || '<div class="oe_structure"></div>';
-        return `
-            <!doctype html>
-            <html>
-                <head>
-                    <base href="${window.location.origin}/">
-                    <style>
-                        body { margin: 0; font-family: ${JSON.stringify(
-                            this.state.selectedFont || "system-ui"
-                        )}; }
-                    </style>
-                </head>
-                <body>
-                    <div id="wrapwrap" class="homepage">
-                        <header>
-                        </header>
-                        <main class="" style="">
-                            ${previewBody}
-                        </main>
-                        <footer>
-                        </footer>
-                    </div>
-                </body>
-            </html>`;
+    applyPreviewFont(iframeDoc = this.previewIframeRef.el?.contentDocument) {
+        if (iframeDoc?.body) {
+            iframeDoc.body.style.fontFamily = this.state.selectedFont || "";
+        }
     }
+
+    get previewUrl() {
+        const params = new URLSearchParams({
+            install_theme: "1",
+            theme_name: this.state.selectedTheme || "theme_default",
+        });
+        return `/website/configurator/preview?${params.toString()}`;
+    }
+
 }
 
 //------------------------------------------------------------------------------
@@ -1128,7 +1100,6 @@ export class Configurator extends Component {
             selectedIndustry: state.selectedIndustry,
             selectedPalette: state.selectedPalette,
             selectedTheme: state.selectedTheme,
-            themePreviewBody: state.themePreviewBody,
             selectedPurpose: state.selectedPurpose,
             formerSelectedPurpose: state.formerSelectedPurpose,
             selectedType: state.selectedType,
