dzdzdzdz1010/OdooStandard

[IMP] discuss: wip recording

addons/mail/tests/discuss/test_rtc.py
@@ -131,6 +131,8 @@ class TestChannelRTC(MailCommon, HttpCase):
                 "res.partner": self._filter_partners_fields(partner_data),
                 "Rtc": {
                     "iceServers": False,
+                    "canRecord": False,
+                    "canRecordVideo": False,
                     "localSession": last_rtc_session_id + 1,
                     "serverInfo": None,
                 },

MoLeap/odooo

addons/mail/tests/discuss/test_rtc.py
@@ -146,6 +146,9 @@ class TestChannelRTC(MailCommon, HttpCase):
                 ),
                 "Rtc": {
                     "iceServers": False,
+                    "canRecordTranscription": False,
+                    "canRecordVideo": False,
+                    "canRecordAudio": False,
                     "localSession": last_rtc_session_id + 1,
                     "serverInfo": None,
                 },