bba80d2aaa654aa4a717a211b5fdd7afeb679978
Stéphane Diez <snd@odoo.com>
2026-03-24 17:31:44 +0100

    [REF] website_sale(_stock): move eCom inventory fields and views
    We are moving allow_out_of_stock_order, available_threshold, show_availability
    and out_of_stock_message to website_sale along with the views.
    
    task 5418128

--- addons/website_sale_stock/__manifest__.py
+++ addons/website_sale_stock/__manifest__.py
@@ -12,7 +12,6 @@ Then it can be made specific at the product level.
     """,
     "depends": ["website_sale", "sale_stock", "stock_delivery"],
     "data": [
-        "views/product_template_views.xml",
         "views/res_config_settings_views.xml",
         "views/sale_order_views.xml",
         "views/website_sale_stock_templates.xml",
