3fd38f48fe7fb5fa5161fc376bd625c5a6f65ed3
David Van Droogenbroeck (DROD) <drod@odoo.com>
2026-03-26 09:47:54 +0100

    [FIX] stock_account: ignore default tax for product revaluation
    Issue
    -----
    If the account on which stock revaluation is registered has a default tax, the
    account moves generated by changing the price of the product include the tax.
    
    For context, the issue was reported because some l10n modules use accounts with
    default taxes (eg l10n_de).
    
    Steps to reproduce
    -----
    - Install a localisation to have default accounts set up
    - Create a product category (Cat1)
    	- Set Inventory Valuation to "Automated"
    	- Set a default tax on the Expense Account
    - Create a product Prod1
    	- Storable
    	- Category set to Cat1
    	- Cost set to 500
    - Update the on hand quantity of Prod1 to 1
    - Change the price of Prod1 to 300
    - Go to Accounting > Accounting > Journal > Journal Entries
    
    --> There is an AM for 200 + tax instead of just 200
    
    Discussion
    -----
    When manually changing the cost of the product, we call
    
    https://github.com/odoo/odoo/blob/09c366dcfa5fe550a1afef813faa79f43767d0a5/addons/stock_account/models/product.py#L354
    
    Where we create the AMs
    
    https://github.com/odoo/odoo/blob/09c366dcfa5fe550a1afef813faa79f43767d0a5/addons/stock_account/models/stock_valuation_layer.py#L294
    
    This creates the corresponding debit/credit AMLs. Afterwards, we go through
    
    https://github.com/odoo/odoo/blob/09c366dcfa5fe550a1afef813faa79f43767d0a5/addons/account/models/account_move.py#L3210
    
    Which calls `_sync_tax_lines` where AMLs are created for the taxes
    
    https://github.com/odoo/odoo/blob/09c366dcfa5fe550a1afef813faa79f43767d0a5/addons/account/models/account_move.py#L3088
    
    Solution
    -----
    1. Add a key in the context when creating the AM, since all of the AMLs are
    created further down the call stack so they inherit the context
    
    2. Add an override of AML `create` in `stock_account` to filter out tax lines
    when the context key is present
    
    -----
    Ticket:
    opw-5929774

--- addons/stock_account/models/stock_valuation_layer.py
+++ addons/stock_account/models/stock_valuation_layer.py
@@ -291,7 +291,7 @@ class StockValuationLayer(models.Model):
             }
             am_vals_list.append(move_vals)
 
-        account_moves = self.env['account.move'].sudo().create(am_vals_list)
+        account_moves = self.env['account.move'].sudo().with_context(is_price_change=True).create(am_vals_list)
         if account_moves:
             account_moves._post()
 
