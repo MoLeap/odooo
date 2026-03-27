odoo/odoo

[IMP] website, *: allow structured routes in list_as_website_content
*= website_sale

Before this commit, `list_as_website_content` was expected to return a
single static title, and the route URL was inferred from the endpoint
routes.

This commit extends the behavior to support callables returning
structured data (list of dicts with `route_title` and `route_url`),
allowing dynamic and explicit website content routes.

task-4714826

addons/website/models/website_technical_page.py
@@ -21,11 +21,13 @@ class WebsiteTechnicalPage(models.Model):
         """
         return self.env["website"].get_client_action(self.website_url)
 
-    # We removed the `@ormcache("routing")` here because `list_as_website_content`
-    # can be defined as a callable for shop/extra_info route. when toggling the "Extra Info"
-    # page option, this attribute calls a method and the available
-    # routes need to be recomputed immediately. Keeping the cache would prevent
-    # the updated route list from being reflected without restarting the server.
+    # We removed the `@ormcache("routing")` here because `list_as_website_content` can be
+    # defined as a callable for conditional routes such as `shop/extra_info`, which are
+    # included when toggling the "Extra Info" page option, as well as for dynamic pages
+    # like `/website/http_error/<status_code>` where the status code is not static; in
+    # these cases the attribute calls a method and the available routes must be
+    # recomputed immediately. Keeping the cache would prevent the updated route list
+    # from being reflected without restarting the server.
     def _get_static_routes(self):
         """
         Returns a set of website content static routes.
@@ -34,15 +36,30 @@ class WebsiteTechnicalPage(models.Model):
         routes = set()
         for rule in self.env["ir.http"].routing_map().iter_rules():
             endpoint = rule.endpoint.routing
-            route_title = endpoint.get("list_as_website_content")
-            if callable(route_title):
-                route_title = route_title(self.env)
-            if route_title:
+            route_data = endpoint.get("list_as_website_content")
+
+            # CASE 1: callable returns structured data
+            if callable(route_data):
+                items = route_data(self.env) or []
+                for item in items:
+                    title = item.get("route_title")
+                    route = item.get("route_url")
+                    if title and route and not dynamic_route_re.search(route):
+                        routes.add((str(title), route))
+                continue
+
+            # CASE 2: static title
+            if route_data:
                 last_static_route = next(
-                    r for r in reversed(endpoint.get("routes", []))
-                    if not dynamic_route_re.search(r)
+                    (
+                        r for r in reversed(endpoint.get("routes", []))
+                        if not dynamic_route_re.search(r)
+                    ),
+                    None,
                 )
-                routes.add((str(route_title), last_static_route))
+                if last_static_route:
+                    routes.add((str(route_data), last_static_route))
+
         return routes
 
     @property

MoLeap/odooo

addons/website/models/website_technical_page.py
@@ -34,15 +34,29 @@ class WebsiteTechnicalPage(models.Model):
         routes = set()
         for rule in self.env["ir.http"].routing_map().iter_rules():
             endpoint = rule.endpoint.routing
-            route_title = endpoint.get("list_as_website_content")
-            if callable(route_title):
-                route_title = route_title(self.env)
-            if route_title:
+            route_data = endpoint.get("list_as_website_content")
+            if not route_data:
+                continue
+            # CASE 1: callable returns structured data
+            if callable(route_data):
+                items = route_data(self.env) or []
+                for item in items:
+                    route_title = item.get("route_title")
+                    route_url = item.get("route_url")
+                    if route_title and route_url and not dynamic_route_re.search(route_url):
+                        routes.add((str(route_title), route_url))
+            # CASE 2: static title
+            else:
                 last_static_route = next(
-                    r for r in reversed(endpoint.get("routes", []))
-                    if not dynamic_route_re.search(r)
+                    (
+                        r for r in reversed(endpoint.get("routes", []))
+                        if not dynamic_route_re.search(r)
+                    ),
+                    None,
                 )
-                routes.add((str(route_title), last_static_route))
+                if last_static_route:
+                    routes.add((str(route_data), last_static_route))
+
         return routes
 
     @property