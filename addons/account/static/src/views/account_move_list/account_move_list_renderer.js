odoo/odoo

[IMP] account: improve duplicate detection
In this commit:
- Highlight the reference in red when it is identical and in yellow when
  references are different. Apply the same logic to the duplicate document
  warning in the form view.
- In the form view duplicate warning, use the Bill Reference if available, then
  the Move Name, then "Draft".
- Yellow warnings are restricted to Draft moves; only red warnings remain
  visible once Posted.

task-5916255

addons/account/static/src/views/account_move_list/account_move_list_renderer.js
@@ -12,7 +12,11 @@ export class AccountMoveListRenderer extends FileUploadListRenderer {
     getCellClass(column, record) {
         const classNames = super.getCellClass(column, record);
         if (column.name === 'ref' && record.data.duplicated_ref_ids && record.data.duplicated_ref_ids.count !== 0) {
-            return `${classNames} table-warning`;
+            if (record.data.is_exact_ref_duplicate) {
+                return `${classNames} table-danger`;
+            } else if (record.data.state === 'draft') {
+                return `${classNames} table-warning`;
+            }
         }
         return classNames;
     }

MoLeap/odooo

X-original-commit: a11ca1733907a401bafcd3adb24fe1a09be51ee5

addons/account/static/src/views/account_move_list/account_move_list_renderer.js
@@ -12,7 +12,11 @@ export class AccountMoveListRenderer extends FileUploadListRenderer {
     getCellClass(column, record) {
         const classNames = super.getCellClass(column, record);
         if (column.name === 'ref' && record.data.duplicated_ref_ids && record.data.duplicated_ref_ids.count !== 0) {
-            return `${classNames} table-warning`;
+            if (record.data.is_exact_move_duplicate) {
+                return `${classNames} table-danger`;
+            } else if (record.data.state === 'draft') {
+                return `${classNames} table-warning`;
+            }
         }
         return classNames;
     }