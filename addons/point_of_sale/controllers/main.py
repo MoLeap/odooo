odoo/odoo

[FIX] point_of_sale: correctly determine if the browser is offline
Runbot tests will soon be run in dockers with no access to the outside
world, so all their interfaces will be disconnected. The problem is that
the browser considers itself offline when no interface is connected.
However, in this case, if the Odoo server is still accessible.

This method also makes it possible to run local tests when no connection
is available and an Odoo server is running locally.

A ping is required to verify that the connection to the server is not
possible.

addons/point_of_sale/controllers/main.py
@@ -99,6 +99,10 @@ class PosController(PortalAccount):
         response.headers['Cache-Control'] = 'no-store'
         return response
 
+    @http.route(['/pos/ping'], type='jsonrpc', auth='user')
+    def pos_ping(self):
+        return {'response': 'pong'}
+
     @http.route('/pos/sale_details_report', type='http', auth='user')
     def print_sale_details(self, date_start=False, date_stop=False, **kw):
         r = request.env['report.point_of_sale.report_saledetails']

MoLeap/odooo

addons/point_of_sale/controllers/main.py
@@ -77,6 +77,10 @@ class PosController(PortalAccount):
         response.headers['Cache-Control'] = 'no-store'
         return response
 
+    @http.route(['/pos/ping'], type='json', auth='user')
+    def pos_ping(self):
+        return {'response': 'pong'}
+
     @http.route('/pos/sale_details_report', type='http', auth='user')
     def print_sale_details(self, date_start=False, date_stop=False, **kw):
         r = request.env['report.point_of_sale.report_saledetails']