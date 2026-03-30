38a4e433dbacaf459f9f242e5c9b6672eb5dea50
romo <romo@odoo.com>
2026-03-30 17:03:32 +0200

    [FIX] website: prevent animation replay when previewing options
    Steps to reproduce the issue :
    - Drop a s_title snippet
    - Add an "onAppearance" animation on the text
    - Write something within the text
    => Every time the text change, the animation restart
    
    The animation should not restart since changing the content of an
    animated text makes it flicker. This issue happens because the animation
    interaction restarts everytime the content within the animated element
    is changed. To fix this issue, we restart the interaction only if some
    specifics attributes were changed.
    
    Steps to reproduce the issue :
    - Drop a s_three_columns snippet
    - Add an "onAppearance" animation on one of the cards
    - Hover the buttons of the option "Content Width"
    => The animation restart everytime the preview start/end
    
    The animation should not restart in this case, as it can disturb the
    users when they are previewing another option. This issue happens
    because the change on "animation-name" impacted the history. Therefore,
    when reverting a preview, the history would also replay the change on
    "animation-name" and restart the animation.
    
    task-5150422

--- addons/website/static/src/interactions/animation.edit.js
+++ addons/website/static/src/interactions/animation.edit.js
@@ -1,15 +1,24 @@
 import { Animation } from "./animation";
 import { registry } from "@web/core/registry";
 
-const AnimationEdit = I => class extends I {
-    destroy() {
-        // We remove the "o_animate_preview" class here because it is added when
-        // an animation is selected in the options, and the "Animation"
-        // interaction considers it as part of the initial state. We remove it
-        // here because otherwise it is added back when exiting edit mode.
-        this.el.classList.remove("o_animate_preview");
-    }
-};
+const AnimationEdit = (I) =>
+    class extends I {
+        destroy() {
+            // We remove the "o_animate_preview" class here because it is added when
+            // an animation is selected in the options, and the "Animation"
+            // interaction considers it as part of the initial state. We remove it
+            // here because otherwise it is added back when exiting edit mode.
+            this.el.classList.remove("o_animate_preview");
+        }
+
+        getConfigurationSnapshot() {
+            return JSON.stringify({
+                isAnimateOnScroll: this.el.classList.contains("o_animate_on_scroll"),
+                isAnimateOnScrollOut: this.el.classList.contains("o_animate_out"),
+                isAnimateBothScroll: this.el.classList.contains("o_animate_both_scroll"),
+            });
+        }
+    };
 
 registry
     .category("public.interactions.edit")
