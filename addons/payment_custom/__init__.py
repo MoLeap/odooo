c7495ebaa25029831c8aa4737770f2632f0a603b
Patricia Daloze (pda) <pda@odoo.com>
2026-03-31 13:55:28 +0200

    [IMP] fixes for review (2)

--- addons/payment_custom/__init__.py
+++ addons/payment_custom/__init__.py
@@ -6,7 +6,6 @@ from odoo.addons.payment import reset_payment_provider, setup_provider
 
 def post_init_hook(env):
     setup_provider(env, "custom", custom_mode="wire_transfer")
-    env.ref("payment_custom.cron_auto_confirm_paid_wire_transfer_txs").active = True
 
 
 def uninstall_hook(env):
