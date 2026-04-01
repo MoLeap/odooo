fleetapps/odoo3

[IMP] html_editor,website_*: improve media dialog video
Unify and rework the multiple different ways we used to match external platform video in the editor and website.

TPVP : third party video providers (YouTube, Vimeo, Intagram, ...)

- Refactor the 'get_video_source_data' python util into a JavaScript equivalent to avoid a lot RPC calls.
- Remove the '/web_editor/video_url/data' python controller which became obsolete.
- Encapsulate supported TPVP regexes and settings into their own classes.
- Remove TPVP thumbnail url definitions from the 'get_video_thumbnail' html_editor util.
- Modify 'product_image.py' model to expect thumbnailUrl in record creation, instead of 'get_video_thumbnail' util calculating it based on video ID.
- Create a new way to parse url and determine if they are from a TPVP then get all the necessary data from `TPVPClass.getVideoUrlData()`.
- Refactor the `VideoSelector` component, used by the media dialog, to be simpler and use the new `getVideoUrlData` mechanism.

task-5318096

addons/html_editor/static/src/main/media/video/vimeo.js
@@ -1,0 +1,65 @@
+import { ThirdPartyVideoAbstract } from "@html_editor/main/media/video/third_party_video_abstract";
+import { encodeOptionsToParams } from "@html_editor/main/media/video/utils";
+
+export class Vimeo extends ThirdPartyVideoAbstract {
+    static id = "vimeo";
+    static name = "Vimeo";
+
+    static urlMatcher =
+        /^(?:(?:https?:)?\/\/)?(player.)?vimeo.com\/([a-z]*\/)?(?<id>[^?]+)(?:\/(?<hash>[^?]+))?(?:\?(?<params>\S+))?$/i;
+
+    static optionsConfig = {
+        startFrom: { default: 0, type: Number },
+        autoplay: { default: false, type: Boolean, params: ["autoplay"] },
+        muted: { default: false, type: Boolean, params: ["muted"] },
+    };
+    /**
+     * Returns the embed url for a vimeo video.
+     *
+     * @param {string} videoId
+     * @param {Object} options
+     * @return {string} url
+     */
+    static getEmbedUrl(videoId, options = {}) {
+        const params = encodeOptionsToParams(options, Vimeo.optionsConfig);
+        let embedUrl = `https://player.vimeo.com/video/${videoId}${params ? "?" + params : ""}`;
+        if (options.startFrom) {
+            embedUrl += `#t=${options.startFrom}`;
+        }
+        return embedUrl;
+    }
+    /**
+     * Returns the url for the thumbnail image of the video.
+     *
+     * @param {string} videoId
+     * @return {Promise[string]} url
+     */
+    static async getThumbnailUrl(videoId) {
+        const apiResponse = await fetch(
+            `https://vimeo.com/api/oembed.json?url=https://vimeo.com/${encodeURIComponent(videoId)}`
+        );
+        if (!apiResponse.ok) {
+            console.warn(
+                `Failed to fetch thumbnail for vimeo video ${videoId} with status ${apiResponse.status}`
+            );
+            return "";
+        }
+        const data = await apiResponse.json();
+        return data.thumbnail_url || "";
+    }
+
+    /**
+     * Used to dynamicaly unit test this video provider with various urls,
+     * all the urls in this object should be parsable wy the urlMatcher.
+     *
+     * @see /addons/html_editor/static/tests/media/*.test.js
+     * */
+    static unitTestUrls = {
+        base: "https://vimeo.com/395399735",
+        unlisted: "https://vimeo.com/795669787/0763fdb816", // Not sure if this is still working
+        embed: "https://player.vimeo.com/video/395399735",
+        embedUnlisted: "https://player.vimeo.com/video/795669787?h=0763fdb816",
+        params: "vimeo.com/395399735?autoplay=1#t=62",
+        embedParams: "https://player.vimeo.com/video/395399735?autoplay=1#t=62",
+    };
+}

MoLeap/odooo

addons/html_editor/static/src/main/media/video/vimeo.js
@@ -1,0 +1,65 @@
+import { ThirdPartyVideoAbstract } from "@html_editor/main/media/video/third_party_video_abstract";
+import { encodeOptionsToParams, BooleanInt } from "@html_editor/main/media/video/utils";
+
+export class Vimeo extends ThirdPartyVideoAbstract {
+    static id = "vimeo";
+    static name = "Vimeo";
+
+    static urlMatcher =
+        /^(?:(?:https?:)?\/\/)?(player.)?vimeo.com\/([a-z]*\/)?(?<id>[^?]+)(?:\/(?<hash>[^?]+))?(?:\?(?<params>\S+))?$/i;
+
+    static optionsConfig = {
+        startFrom: { default: 0, type: Number },
+        autoplay: { default: false, type: BooleanInt, params: ["autoplay"] },
+        muted: { default: false, type: BooleanInt, params: ["muted"] },
+    };
+    /**
+     * Returns the embed url for a vimeo video.
+     *
+     * @param {string} videoId
+     * @param {Object} options
+     * @return {string} url
+     */
+    static getEmbedUrl(videoId, options = {}) {
+        const params = encodeOptionsToParams(options, Vimeo.optionsConfig);
+        let embedUrl = `https://player.vimeo.com/video/${videoId}${params ? "?" + params : ""}`;
+        if (options.startFrom) {
+            embedUrl += `#t=${options.startFrom}`;
+        }
+        return embedUrl;
+    }
+    /**
+     * Returns the url for the thumbnail image of the video.
+     *
+     * @param {string} videoId
+     * @return {Promise[string]} url
+     */
+    static async getThumbnailUrl(videoId) {
+        const apiResponse = await fetch(
+            `https://vimeo.com/api/oembed.json?url=https://vimeo.com/${encodeURIComponent(videoId)}`
+        );
+        if (!apiResponse.ok) {
+            console.warn(
+                `Failed to fetch thumbnail for vimeo video ${videoId} with status ${apiResponse.status}`
+            );
+            return "";
+        }
+        const data = await apiResponse.json();
+        return data.thumbnail_url || "";
+    }
+
+    /**
+     * Used to dynamicaly unit test this video provider with various urls,
+     * all the urls in this object should be parsable wy the urlMatcher.
+     *
+     * @see /addons/html_editor/static/tests/media/*.test.js
+     * */
+    static unitTestUrls = {
+        base: "https://vimeo.com/395399735",
+        unlisted: "https://vimeo.com/795669787/0763fdb816", // Not sure if this is still working
+        embed: "https://player.vimeo.com/video/395399735",
+        embedUnlisted: "https://player.vimeo.com/video/795669787?h=0763fdb816",
+        params: "vimeo.com/395399735?autoplay=1#t=62",
+        embedParams: "https://player.vimeo.com/video/395399735?autoplay=1#t=62",
+    };
+}