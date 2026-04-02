c2f5e1834454a1d6fd7ec099b75c8929fe5612ff
Hadi El Yakhni <hael@odoo.com>
2026-04-02 12:57:35 +0200

    [FIX] pos_self_order: translate custom link names for available languages
    Steps to reproduce:
    -------------------
    1. Enable self ordering with default language set to French
    2. Open the self-order interface
    
    -> The "Order Now" button label is still in English.
    
    Why the issue:
    --------------
    The "Order Now" link is created dynamically in Python, so its translations
    are not automatically populated from the po files. Only the language active
    at creation time (typically English) is stored.
    
    The fix:
    --------
    In `_prepare_self_order_custom_btn`, after finding or creating that link,
    populate the translations for it, for all available self-ordering
    languages, using `get_translation`. We skip updating the translation if
    it's already different from the source (in that case, it's probably
    customized by the user).
    
    Notes:
    ------
    `get_translation` was directly used because we cannot use `env._`
    function as follows: `env._(link.name)` as that will throw a linting
    error: "Bad usage of _, _lt function. (E8502)".
    
    See: https://www.odoo.com/documentation/master/developer/howtos/translations.html#variables
    
    opw-6088248

--- addons/pos_self_order/models/pos_config.py
+++ addons/pos_self_order/models/pos_config.py
@@ -10,6 +10,7 @@ from urllib.parse import unquote
 from odoo.exceptions import UserError, ValidationError, AccessError
 
 from odoo import api, fields, models, _, service
+from odoo.tools.translate import get_translation
 from odoo.tools import file_open, split_every
 
 
@@ -151,17 +152,25 @@ class PosConfig(models.Model):
 
     def _prepare_self_order_custom_btn(self):
         for record in self:
-            exists = record.env['pos_self_order.custom_link'].search_count([
+            links = record.env['pos_self_order.custom_link'].search([
                 ('pos_config_ids', 'in', record.id),
                 ('url', '=', f'/pos-self/{record.id}/products')
             ])
 
-            if not exists:
-                record.env['pos_self_order.custom_link'].create({
+            if not links:
+                links = record.env['pos_self_order.custom_link'].create({
                     'name': _('Order Now'),
                     'url': f'/pos-self/{record.id}/products',
                     'pos_config_ids': [(4, record.id)],
                 })
+
+            for link in links.filtered(lambda l: l.name == 'Order Now'):
+                translations = {
+                    lang.code: get_translation('pos_self_order', lang.code, link.name, ())
+                    for lang in record.self_ordering_available_language_ids
+                    if link.with_context(lang=lang.code).name == link.name
+                }
+                link.update_field_translations('name', translations)
 
     def write(self, vals):
         self._prepare_self_order_splash_screen([vals])
