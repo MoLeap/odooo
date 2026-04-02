odoo/odoo

[FIX] mail: load default template body in full composer
Issue:
When opening the full composer from the chatter, the body of the default email
template is not loaded. Only the subject line from the template appears,
while the body remains empty or contains only the user's signature.

Steps to reproduce:
1. Install `contact`
2. Open any contact form.
3. In the chatter, click 'Send message' and then expand button
4. Write a something in body, then save this as a new template.
5. Set this new template as the default (using Debug Mode > Set Default Values).
6. Click 'Send message' in the chatter,
7. Click the 'Full composer' (expand) button without typing anything.

Observed behavior:
The full composer opens with the correct subject from the default template,
but the body is empty.

Cause:
The `onClickFullComposer` method always passes a `default_body` value
in the context to the mail.compose.message wizard. Even if the chatter input is empty

Solution:
Forward isBodyEmpty in the context from onClickFullComposer.
If the user typed content, do nothing. If the body is empty and a default template is available,
allow the backend to apply the default template by removing default_body.

opw-5405056

addons/mail/static/src/core/common/composer.js
@@ -590,6 +590,7 @@ export class Composer extends Component {
             default_res_ids: [this.thread.id],
             default_subtype_xmlid: this.props.type === "note" ? "mail.mt_note" : "mail.mt_comment",
             mail_post_autofollow: this.thread.hasWriteAccess,
+            is_body_empty: !body || body.trim().length === 0,
         };
         const action = {
             name: this.props.type === "note" ? _t("Log note") : _t("Compose Email"),

MoLeap/odooo

X-original-commit: fb2551a67f520929827b939e3baf0dc8de3bd19e

addons/mail/static/src/core/common/composer.js
@@ -676,6 +676,7 @@ export class Composer extends Component {
             default_res_ids: [this.thread.id],
             default_subtype_xmlid: this.props.type === "note" ? "mail.mt_note" : "mail.mt_comment",
             clicked_on_full_composer: true,
+            body_contains_signature_only: !body || body.trim().length === 0,
             // Changed in 18.2+: finally get rid of autofollow, following should be done manually
             ...this.fullComposerAdditionalContext,
         };