odoo/odoo

[FIX] html_editor: skip syntax highlighting for very large pre blocks
Purpose of this commit:

- Large `<pre>` blocks that use syntax highlighting can severely impact
performance when opening a task or when applying transformations
(e.g. converting to paragraph). In extreme cases, this can even crash
the browser.
- To avoid this, `<pre>` elements whose text content exceeds the syntax
highlighting size limit are no longer converted into syntax-highlighted
embedded code components (with textarea and Prism). Instead, they are
kept as plain <pre> elements.

task-5907330

addons/html_editor/static/src/others/embedded_component_plugin.js
@@ -31,7 +31,7 @@ export class EmbeddedComponentPlugin extends Plugin {
         restore_savepoint_handlers: () => this.handleComponents(this.editable),
         history_reset_handlers: () => this.handleComponents(this.editable),
         history_reset_from_steps_handlers: () => this.handleComponents(this.editable),
-        step_added_handlers: ({ stepCommonAncestor }) => this.handleComponents(stepCommonAncestor),
+        step_added_handlers: () => this.handleComponents(this.editable),
         external_step_added_handlers: () => this.handleComponents(this.editable),
 
         serializable_descendants_processors: this.processDescendantsToSerialize.bind(this),

MoLeap/odooo

addons/html_editor/static/src/others/embedded_component_plugin.js
@@ -30,8 +30,7 @@ export class EmbeddedComponentPlugin extends Plugin {
         on_savepoint_restored_handlers: () => this.handleComponents(this.editable),
         on_history_reset_handlers: () => this.handleComponents(this.editable),
         on_history_reset_from_steps_handlers: () => this.handleComponents(this.editable),
-        on_step_added_handlers: ({ stepCommonAncestor }) =>
-            this.handleComponents(stepCommonAncestor),
+        on_step_added_handlers: () => this.handleComponents(this.editable),
         on_external_step_added_handlers: () => this.handleComponents(this.editable),
 
         /** Processors */