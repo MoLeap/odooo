odoo/odoo

[IMP] mail,*: preserve the message formatting in various message preview
*=html_editor

Before this commit,
The message preview that was shown in various parts of the applications shows
only the inline version of the message with formatting being stripped out.
This gave false impression about how the original message actually is...

This commit, retains the message's original formatting and making it inline
by adding appropriate spaces in place of <br> and in between two block level
elements.

Task-5263284

addons/mail/static/src/utils/common/format.js
@@ -10,6 +10,7 @@ import { getInnerHtml, getOuterHtml } from "@mail/utils/common/html";
 
 import { htmlEscape, markup } from "@odoo/owl";
 
+import { blockTagNames } from "@html_editor/utils/blocks";
 import { router } from "@web/core/browser/router";
 import { loadEmoji, loader } from "@web/core/emoji_picker/emoji_picker";
 import { normalize } from "@web/core/l10n/utils";
@@ -368,6 +369,30 @@ export function htmlToTextContentInline(htmlString) {
 export function convertBrToLineBreak(str) {
     str = htmlReplace(str, /<br\s*\/?>/gi, () => "\n");
     return createDocumentFragmentFromContent(str).body.textContent;
+}
+
+/**
+ * @param {string|ReturnType<markup>} richbody
+ * @returns {ReturnType<markup>}
+ */
+export function normalizeBrAndBlocks(richbody) {
+    const doc = createDocumentFragmentFromContent(richbody);
+    const body = doc.body;
+    const lineBreaks = body.querySelectorAll("br");
+    const blockChildren = Array.from(body.children);
+    if (lineBreaks.length || blockChildren.length > 1) {
+        for (const br of lineBreaks) {
+            br.replaceWith(body.ownerDocument.createTextNode("\u00A0"));
+        }
+        for (let i = blockChildren.length - 1; i > 0; i--) {
+            const prev = blockChildren[i - 1];
+            const curr = blockChildren[i];
+            if (blockTagNames.includes(prev.tagName) && blockTagNames.includes(curr.tagName)) {
+                body.insertBefore(body.ownerDocument.createTextNode("\u00A0"), curr);
+            }
+        }
+    }
+    return markup(body.innerHTML.trim()) ?? "";
 }
 
 export function cleanTerm(term) {

MoLeap/odooo

addons/mail/static/src/utils/common/format.js
@@ -10,6 +10,7 @@ import { getInnerHtml, getOuterHtml } from "@mail/utils/common/html";
 
 import { htmlEscape, markup } from "@odoo/owl";
 
+import { blockTagNames } from "@html_editor/utils/blocks";
 import { router } from "@web/core/browser/router";
 import { emojiLoader } from "@web/core/emoji_picker/emoji_loader";
 import { formatList, normalize } from "@web/core/l10n/utils";
@@ -376,6 +377,30 @@ export function convertBrToLineBreak(str) {
 
 export function convertLineBreakToBr(str) {
     return htmlReplace(str, /(\r|\n)/g, () => markup`<br/>`);
+}
+
+/**
+ * @param {string|ReturnType<markup>} richbody
+ * @returns {ReturnType<markup>}
+ */
+export function htmlToHtmlInline(richbody) {
+    const doc = createDocumentFragmentFromContent(richbody);
+    const body = doc.body;
+    const lineBreaks = body.querySelectorAll("br");
+    const blockChildren = Array.from(body.children);
+    if (lineBreaks.length || blockChildren.length > 1) {
+        for (const br of lineBreaks) {
+            br.replaceWith(body.ownerDocument.createTextNode("\u00A0"));
+        }
+        for (let i = blockChildren.length - 1; i > 0; i--) {
+            const prev = blockChildren[i - 1];
+            const curr = blockChildren[i];
+            if (blockTagNames.includes(prev.tagName) && blockTagNames.includes(curr.tagName)) {
+                body.insertBefore(body.ownerDocument.createTextNode("\u00A0"), curr);
+            }
+        }
+    }
+    return htmlTrim(getInnerHtml(body)) ?? "";
 }
 
 export function cleanTerm(term) {