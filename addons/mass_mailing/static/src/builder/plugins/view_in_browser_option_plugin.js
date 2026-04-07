2211154d64f92ac65aeff9a6f266577eff72ac35
Haroune Hassine (Hahas) <hahas@odoo.com>
2026-04-07 12:09:26 +0200

    fixup! [IMP] mass_mailing: make view in browser a builder option

--- addons/mass_mailing/static/src/builder/plugins/view_in_browser_option_plugin.js
+++ addons/mass_mailing/static/src/builder/plugins/view_in_browser_option_plugin.js
@@ -64,7 +64,7 @@ export class ToggleViewInBrowserAction extends BuilderAction {
     }
 
     isApplied() {
-        return this.dependencies["viewInBrowserOptionPlugin"].getViewInBrowserLink();
+        return Boolean(this.dependencies["viewInBrowserOptionPlugin"].getViewInBrowserLink());
     }
 }
 
