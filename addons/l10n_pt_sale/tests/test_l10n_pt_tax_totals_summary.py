InityASalas/odoo

[IMP] sale, l10n_pt_sale: requirements for PT certification
This commit implements the following requirements for Portugal - Sale.

AT Series
----
Documents must belong to an AT Series, communicated to the Autoridade
Tributária. Quotations and Sales Orders fall under two different series for
the PT authorities. To handle this difference, a new sale.order record is
created when the quotation is confirmed, which belongs to the Sales Order
series. The original quotation remains under the Quotations series.

Hash
----
A document which, due to its characteristics (identification of the customer
and value of the goods traded or services provided), can be handed over to
the customer for checking should be hashed.

As such, Sales Orders to be presented to clients should be hashed. For this,
the following fields are used: `date_order`, `l10n_pt_hashed_on`, `name` as
well as the hash of the previous `sale.order`. These are sent to IAP, where
we'll use a private key (belonging to Odoo) to sign the documents.
To avoid too many RPC calls to IAP, the documents are signed only when
printed. At that point, pickings are batched such that we have only once
RPC call to IAP.

ATCUD
-----
The ATCUD code is a unique code that represents a unique document in
Portugal. It's the simple concatenation of the AT series and the
sequence number within that series.

Also, a hash report has been added. It uses the public key returned by
IAP to verify the chain of hashes of the Sales Orders.

Layout requirements
-------------------

- Documents must indicate "Este documento não serve de fatura"

- Signed documents must indicate
"E.g.: HASH - Processado por programa certificado n.º 0000/AT"
where HASH is the 1st, 11th, 21st, 31st chars of the hash/signature.

- Non-signed documents must indicate the ATCUD and
"Emitido por programa certificado n.º 0000/AT"

Move Tests from sales
-------------
In #191402, tests for the tax summary with round globally for Portugal
were added in sales, to ensure the cer

addons/l10n_pt_sale/tests/test_l10n_pt_tax_totals_summary.py
@@ -1,0 +1,30 @@
+from odoo import Command
+from odoo.tests import tagged
+
+from odoo.addons.l10n_pt.tests.test_l10n_pt_tax_totals_summary import TestL10nPtTaxTotalsSummary
+from odoo.addons.sale.tests.common import TestTaxCommonSale
+from odoo.addons.account.tests.common import TestTaxCommon
+
+
+@tagged('post_install', '-at_install', 'post_install_l10n')
+class TestTaxesTaxTotalsSummarySale(TestTaxCommonSale, TestL10nPtTaxTotalsSummary):
+
+    @classmethod
+    @TestTaxCommon.setup_country('pt')
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env['l10n_pt.at.series'].create({
+            'name': 'Test',
+            'company_id': cls.company_data['company'].id,
+            'training_series': True,
+            'at_series_line_ids': [
+                Command.create({'type': 'quotation', 'prefix': 'OR', 'at_code': 'AT-TESTQUOT'}),
+                Command.create({'type': 'sales_order', 'prefix': 'NE', 'at_code': 'AT-TESTSO'}),
+            ],
+        })
+
+    def test_taxes_l10n_pt_sale_orders(self):
+        for test_index, document, expected_values in self._test_taxes_l10n_pt():
+            with self.subTest(test_index=test_index):
+                sale_order = self.convert_document_to_sale_order(document)
+                self.assert_sale_order_tax_totals_summary(sale_order, expected_values)

MoLeap/odooo

addons/l10n_pt_sale/tests/test_l10n_pt_tax_totals_summary.py
@@ -1,0 +1,30 @@
+from odoo import Command
+from odoo.tests import tagged
+
+from odoo.addons.l10n_pt_certification.tests.test_taxes_tax_totals_summary import TestTaxesTaxTotalsSummaryL10nPt
+from odoo.addons.sale.tests.common import TestTaxCommonSale
+from odoo.addons.account.tests.common import TestTaxCommon
+
+
+@tagged('post_install', '-at_install', 'post_install_l10n')
+class TestL10nPtTaxTotalsSummarySale(TestTaxCommonSale, TestTaxesTaxTotalsSummaryL10nPt):
+
+    @classmethod
+    @TestTaxCommon.setup_country('pt')
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env['l10n_pt.at.series'].create({
+            'name': 'Test',
+            'company_id': cls.company_data['company'].id,
+            'training_series': True,
+            'at_series_line_ids': [
+                Command.create({'type': 'quotation', 'prefix': 'OR', 'at_code': 'AT-TESTQUOT'}),
+                Command.create({'type': 'sales_order', 'prefix': 'NE', 'at_code': 'AT-TESTSO'}),
+            ],
+        })
+
+    def test_taxes_l10n_pt_sale_orders(self):
+        for test_index, document, expected_values in self._test_taxes_l10n_pt():
+            with self.subTest(test_index=test_index):
+                sale_order = self.convert_document_to_sale_order(document)
+                self.assert_sale_order_tax_totals_summary(sale_order, expected_values)