odoo/odoo

[IMP] base: adapt code to use new model ir.access

odoo/addons/base/models/ir_access.py
@@ -164,6 +164,37 @@ class IrAccess(models.Model):
             domain = expression.AND([domain, *restrictions])
         return domain
 
+    @ormcache('self.env.uid', 'model_name', 'operation', 'tuple(self._get_access_context())')
+    def _get_restriction_domain(self, model_name: str, operation: str) -> list:
+        """ Return the domain that combines the restrictions to perform
+        ``operation`` on the records of ``model_name``.
+        """
+        assert operation in OPERATIONS, "Invalid access operation"
+
+        domain = [
+            ('model_id', '=', self.env['ir.model']._get_id(model_name)),
+            ('group_id', '=', False),
+            (f'for_{operation}', '=', True),
+            ('active', '=', True),
+        ]
+        accesses = self.sudo().search_fetch(domain, ['group_id', 'domain'], order='id')
+
+        # collect restrictions
+        restrictions = []
+        eval_context = self._eval_context()
+        for access in accesses:
+            domain = safe_eval(access.domain, eval_context) if access.domain else []
+            restrictions.append(domain)
+
+        # add access for parent models as restrictions
+        for parent_model_name, parent_field_name in self.env[model_name]._inherits.items():
+            domain = self._get_access_domain(parent_model_name, operation)
+            if domain is None:
+                return [expression.FALSE_LEAF]
+            restrictions.append([(parent_field_name, 'any', domain)])
+
+        return expression.AND(restrictions)
+
     def _get_access_records(self, model_name: str, operation: str):
         """ Returns all the accesses matching the given model for the operation
         for the current user.
@@ -195,6 +226,30 @@ class IrAccess(models.Model):
             'company_ids': self.env.companies.ids,
             'company_id': self.env.company.id,
         }
+
+    @ormcache('model_name', 'operation')
+    def _get_access_groups(self, model_name, operation='read'):
+        """ Return the group expression object that represents the users who
+        can perform ``operation`` on model ``model_name``.
+        """
+        assert operation in OPERATIONS, "Invalid access operation"
+        accesses = self._get_access_records(model_name, operation)
+        group_definitions = self.env['res.groups']._get_group_definitions()
+        return group_definitions.from_ids(accesses.group_id.ids)
+
+    def _get_allowed_models(self, operation='read') -> set[str]:
+        """ Return all the models for which the given operation is possible. """
+        assert operation in OPERATIONS, "Invalid access operation"
+
+        group_ids = self.env.user._get_group_ids()
+        domain = [('access_ids', 'any', [
+            ('active', '=', True),
+            (f'for_{operation}', '=', True),
+            ('group_id', 'in', group_ids),
+        ])]
+        models = self.sudo().env['ir.model'].search_fetch(domain, ['model'], order='id')
+
+        return set(models.mapped('model'))
 
     def _make_access_error(self, records, operation: str) -> AccessError:
         """ Return the exception to be raised in case of access error.

odoo/addons/base/models/res_users.py
@@ -182,9 +182,6 @@ class ResGroups(models.Model):
 
     name = fields.Char(required=True, translate=True)
     users = fields.Many2many('res.users', 'res_groups_users_rel', 'gid', 'uid')
-    model_access = fields.One2many('ir.model.access', 'group_id', string='Access Controls', copy=True)
-    rule_groups = fields.Many2many('ir.rule', 'rule_group_rel',
-        'group_id', 'rule_group_id', string='Rules', domain="[('global', '=', False)]")
     access_ids = fields.One2many('ir.access', 'group_id', string="Access Rules", copy=True)
     menu_access = fields.Many2many('ir.ui.menu', 'ir_ui_menu_group_rel', 'gid', 'menu_id', string='Access Menu')
     view_access = fields.Many2many('ir.ui.view', 'ir_ui_view_group_rel', 'group_id', 'view_id', string='Views')
@@ -276,7 +273,7 @@ class ResGroups(models.Model):
         # field 'share' depends on method has_group()
         # DLE P139
         if self.ids:
-            self.env['ir.model.access'].call_cache_clearing_methods()
+            self.env['ir.access']._clear_caches()
         return super().write(vals)
 
     def _ensure_xml_id(self):
@@ -410,8 +407,6 @@ class ResUsers(models.Model):
 
     accesses_count = fields.Integer('# Access Rights', help='Number of access rights that apply to the current user',
                                     compute='_compute_accesses_count', compute_sudo=True)
-    rules_count = fields.Integer('# Record Rules', help='Number of record rules that apply to the current user',
-                                 compute='_compute_accesses_count', compute_sudo=True)
     groups_count = fields.Integer('# Groups', help='Number of groups that apply to the current user',
                                   compute='_compute_accesses_count', compute_sudo=True)
 
@@ -576,8 +571,7 @@ class ResUsers(models.Model):
     def _compute_accesses_count(self):
         for user in self:
             groups = user.groups_id
-            user.accesses_count = len(groups.model_access)
-            user.rules_count = len(groups.rule_groups)
+            user.accesses_count = len(groups.access_ids)
             user.groups_count = len(groups)
 
     @api.depends('res_users_settings_ids')
@@ -830,7 +824,7 @@ class ResUsers(models.Model):
                 super(ResUsers, users).write(vals)
             # clear caches linked to the users
             if self.ids:
-                self.env['ir.model.access'].call_cache_clearing_methods()
+                self.env['ir.access']._clear_caches()
 
         # per-method / per-model caches have been removed so the various
         # clear_cache/clear_caches methods pretty much just end up calling
@@ -1300,22 +1294,10 @@ class ResUsers(models.Model):
         return {
             'name': _('Access Rights'),
             'view_mode': 'list,form',
-            'res_model': 'ir.model.access',
+            'res_model': 'ir.access',
             'type': 'ir.actions.act_window',
             'context': {'create': False, 'delete': False},
-            'domain': [('id', 'in', self.groups_id.model_access.ids)],
-            'target': 'current',
-        }
-
-    def action_show_rules(self):
-        self.ensure_one()
-        return {
-            'name': _('Record Rules'),
-            'view_mode': 'list,form',
-            'res_model': 'ir.rule',
-            'type': 'ir.actions.act_window',
-            'context': {'create': False, 'delete': False},
-            'domain': [('id', 'in', self.groups_id.rule_groups.ids)],
+            'domain': [('group_id', 'in', self.groups_id.ids)],
             'target': 'current',
         }
 

MoLeap/odooo

odoo/addons/base/models/ir_access.py
@@ -186,6 +186,40 @@ class IrAccess(models.Model):
 
         return Domain.OR(permissions) & Domain.AND(restrictions)
 
+    @ormcache('self.env.uid', 'model_name', 'operation', 'include_inherits', 'tuple(self._get_access_context())')
+    def _get_restriction_domain_for(self, model_name: str, operation: str, *, include_inherits=True) -> Domain:
+        """ Return the domain that combines the restrictions to perform
+        ``operation`` on the records of ``model_name``.
+        """
+        assert operation in OPERATIONS, "Invalid access operation"
+
+        domain = Domain([
+            ('model_id', '=', self.env['ir.model']._get_id(model_name)),
+            ('group_id', '=', False),
+            (f'for_{operation}', '=', True),
+            ('active', '=', True),
+        ])
+        accesses = self.sudo().search_fetch(domain, ['group_id', 'domain'], order='id')
+
+        # collect restrictions
+        eval_context = self._eval_context()
+        restrictions = [
+            Domain(safe_eval(access.domain, eval_context)) if access.domain else Domain.TRUE
+            for access in accesses
+        ]
+
+        # add access for parent models as restrictions
+        if include_inherits:
+            for parent_model_name, parent_field_name in self.env[model_name]._inherits.items():
+                domain = self._get_restriction_domain_for(parent_model_name, operation)
+                if domain.is_false():
+                    return Domain.FALSE
+                if domain.is_true():
+                    continue
+                restrictions.append(Domain(parent_field_name, 'any', domain))
+
+        return Domain.AND(restrictions)
+
     def _get_records_for(self, model_name: str, operation: str):
         """ Returns all the accesses matching the given model for the operation
         for the current user.
@@ -217,6 +251,41 @@ class IrAccess(models.Model):
             'company_ids': self.env.companies.ids,
             'company_id': self.env.company.id,
         }
+
+    @ormcache('model_name', 'operation')
+    def _get_access_groups(self, model_name, operation='read'):
+        """ Return the group expression object that represents the users who
+        can perform ``operation`` on model ``model_name``.
+        """
+        assert operation in OPERATIONS, "Invalid access operation"
+
+        model_id = self.env['ir.model']._get_id(model_name)
+        domain = [
+            ('model_id', '=', model_id),
+            ('group_id', '!=', False),
+            (f'for_{operation}', '=', True),
+            ('domain', '!=', "[(0, '=', 1)]"),
+            ('active', '=', True),
+        ]
+        accesses = self.sudo().search_fetch(domain, ['group_id'])
+
+        group_definitions = self.env['res.groups']._get_group_definitions()
+        return group_definitions.from_ids(accesses.group_id.ids)
+
+    def _get_allowed_models(self, operation='read') -> set[str]:
+        """ Return all the models for which the given operation is possible. """
+        assert operation in OPERATIONS, "Invalid access operation"
+
+        group_ids = self.env.user._get_group_ids()
+        domain = Domain('access_ids', 'any', [
+            ('group_id', 'in', group_ids),
+            (f'for_{operation}', '=', True),
+            ('domain', '!=', "[(0, '=', 1)]"),
+            ('active', '=', True),
+        ])
+        models = self.sudo().env['ir.model'].search_fetch(domain, ['model'], order='id')
+
+        return set(models.mapped('model'))
 
     def _make_access_error(self, records, operation: str) -> AccessError:
         """ Return the exception to be raised in case of access error.

odoo/addons/base/models/res_users.py
@@ -250,8 +250,6 @@ class ResUsers(models.Model):
 
     accesses_count = fields.Integer('# Access Rights', help='Number of access rights that apply to the current user',
                                     compute='_compute_accesses_count', compute_sudo=True)
-    rules_count = fields.Integer('# Record Rules', help='Number of record rules that apply to the current user',
-                                 compute='_compute_accesses_count', compute_sudo=True)
     groups_count = fields.Integer('# Groups', help='Number of groups that apply to the current user',
                                   compute='_compute_accesses_count', compute_sudo=True)
 
@@ -475,8 +473,7 @@ class ResUsers(models.Model):
     def _compute_accesses_count(self):
         for user in self:
             groups = user.all_group_ids
-            user.accesses_count = len(groups.model_access)
-            user.rules_count = len(groups.rule_groups)
+            user.accesses_count = len(groups.access_ids)
             user.groups_count = len(groups)
 
     @api.depends('res_users_settings_ids')
@@ -1128,22 +1125,10 @@ class ResUsers(models.Model):
         return {
             'name': _('Access Rights'),
             'view_mode': 'list,form',
-            'res_model': 'ir.model.access',
+            'res_model': 'ir.access',
             'type': 'ir.actions.act_window',
             'context': {'create': False, 'delete': False},
-            'domain': [('id', 'in', self.all_group_ids.model_access.ids)],
-            'target': 'current',
-        }
-
-    def action_show_rules(self):
-        self.ensure_one()
-        return {
-            'name': _('Record Rules'),
-            'view_mode': 'list,form',
-            'res_model': 'ir.rule',
-            'type': 'ir.actions.act_window',
-            'context': {'create': False, 'delete': False},
-            'domain': [('id', 'in', self.all_group_ids.rule_groups.ids)],
+            'domain': [('group_id', 'in', self.all_group_ids.ids)],
             'target': 'current',
         }
 