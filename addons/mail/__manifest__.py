odoo-dev/odoo

[IMP] mail: store html tracking data in message body
  previously, tracking values were stored in the mail.tracking.value
  model while messages were stored in mail.message.This increased
  database size and caused additional iteration during chatter loading.

 in this commit, all tracking-related values are now stored directly in
 the message body

task-5260614

addons/mail/__manifest__.py
@@ -61,6 +61,7 @@ For more specific needs, you may also assign custom-defined actions
     'website': 'https://www.odoo.com/app/discuss',
     'depends': ['base', 'base_setup', 'bus', 'web_tour', 'html_editor'],
     'data': [
+        'data/mail_tracking_data.xml',
         'data/mail_groups.xml',
         'wizard/mail_activity_schedule_views.xml',
         'wizard/mail_blacklist_remove_views.xml',

MoLeap/odooo

addons/mail/__manifest__.py
@@ -92,10 +92,10 @@ For more specific needs, you may also assign custom-defined actions
         'views/mail_activity_plan_views.xml',
         'views/mail_activity_plan_template_views.xml',
         'views/res_config_settings_views.xml',
+        'data/mail_templates_chatter.xml',
         'data/ir_config_parameter_data.xml',
         'data/res_partner_data.xml',
         'data/mail_message_subtype_data.xml',
-        'data/mail_templates_chatter.xml',
         'data/mail_templates_email_layouts.xml',
         'data/mail_templates_mailgateway.xml',
         'data/discuss_channel_data.xml',